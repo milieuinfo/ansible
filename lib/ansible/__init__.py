46b259e27a344b552bb1c6897af9d1cc04039234
Serge van Ginderachter <serge@vanginderachter.be>
2014-03-12 16:29:26 +0100

    another bugfix release 1.6.0-acd-20140312-0-custombugfixes

--- lib/ansible/__init__.py
+++ lib/ansible/__init__.py
@@ -14,5 +14,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '1.6.0-acd-20140311-1-groupvars-subdirs-patch'
+__version__ = '1.6.0-acd-20140312-0-custombugfixes'
 __author__ = 'Michael DeHaan'
